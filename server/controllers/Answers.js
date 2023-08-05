fa274c1718c42584cbe3aab9a4d631bde8f37f9b
Himanshu Rajput <hiowner00@gmail.com>
2023-08-05 16:30:59 +0530

    share functionality added

---
+++ server/controllers/Answers.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import Question from "../models/Questions.js";
+
+export const postAnswer = async (req, res) => {
+    const { id: _id } = req.params;
+    const { noOfAnswers, answerBody, userAnswered } = req.body;
+    const userId = req.userId;
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(404).send("question unavailable...");
+    }
+  
+    updateNoOfQuestions(_id, noOfAnswers);
+    try {
+      const updatedQuestion = await Question.findByIdAndUpdate(_id, {
+        $addToSet: { answer: [{ answerBody, userAnswered, userId }] },
+      });
+      res.status(200).json(updatedQuestion);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json("error in updating");
+    }
+  };
+
+const updateNoOfQuestions = async (_id, noOfAnswers) => {
+    try {
+        await Question.findByIdAndUpdate(_id, { $set: {'noOfAnswers': noOfAnswers}})
+    } catch (error) {
+        console.log(error)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import  express  from "express";
 import mongoose from "mongoose";
 import cors from 'cors'
 import userRoutes from './routes/users.js'
+import questionRoutes from './routes/Questions.js'
+import answerRoutes from './routes/Answers.js'
 const app = express();
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb", extended: true}))
@@ -14,6 +16,8 @@ app.get('/',(req,res)=> {
 })
 
 app.use('/user', userRoutes)
+app.use('/questions', questionRoutes)
+app.use('/answer', answerRoutes)
 
 const PORT = process.env.PORT || 5000
 

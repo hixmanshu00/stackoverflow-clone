cbe6c304fe5fbda48087497c0a6798a63644caff
Himanshu Rajput <hiowner00@gmail.com>
2023-08-15 22:19:25 +0530

    completed

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Use  this command in both client and server to install dependencies
+npm i
+
+# Use  this command in both client and server to run the server
+npm start
--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ import cors from 'cors'
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
+import dotenv from 'dotenv'
 const app = express();
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb", extended: true}))
 
 app.use(cors());
-
+dotenv.config()
 
 app.get('/',(req,res)=> {
     res.send("This is a stack overflow clone API")
@@ -21,7 +22,7 @@ app.use('/answer', answerRoutes)
 
 const PORT = process.env.PORT || 5000
 
-const URL = 'mongodb+srv://hixmanshu:beginhi2.0@cluster0.vzmut3o.mongodb.net/?retryWrites=true&w=majority'
+const URL = process.env.CONNECTION_URL
 
 mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> {console.log(`server running on port ${PORT}`)}))

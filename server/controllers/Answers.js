4945786e4d3110cbfd9ca50078541fbcb474e62f
Himanshu Rajput <hiowner00@gmail.com>
2023-08-15 22:02:29 +0530

    completed

--- server/controllers/Answers.js
+++ server/controllers/Answers.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import Question from "../models/Questions.js";
+import Questions from "../models/Questions.js";
 
 export const postAnswer = async (req, res) => {
     const { id: _id } = req.params;
-    const { noOfAnswers, answerBody, userAnswered } = req.body;
-    const userId = req.userId;
+    const { noOfAnswers, answerBody, userAnswered, userId } = req.body;
+    // const userId = req.userId;
     if (!mongoose.Types.ObjectId.isValid(_id)) {
       return res.status(404).send("question unavailable...");
     }
@@ -27,4 +28,27 @@ const updateNoOfQuestions = async (_id, noOfAnswers) => {
     } catch (error) {
         console.log(error)
     }
+}
+
+export const deleteAnswer = async(req,res) => {
+  const {id: _id} = req.params;
+  const {answerId, noOfAnswers} = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("Question unavailable...");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(answerId)) {
+    return res.status(404).send("Answer unavailable...");
+  }
+  updateNoOfQuestions(_id,noOfAnswers-1)
+  try {
+    await Questions.updateOne(
+      {_id},
+      {$pull: {'answer': {_id: answerId}}}
+    )
+    res.status(200).json({message: "Successfully deleted..."})
+  } catch (error) {
+    res.status(405).json(error)
+  }
 }
